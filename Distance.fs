f22307c8e08e0972253597f6bed6c00eddb02d4b
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2024-09-02 08:59:12 -0700

    Incorporate A Mutel feedback in Distance
    Use MemoryMarshal instead of Slice

--- Distance.fs
+++ Distance.fs
@@ -4,6 +4,7 @@ namespace SIMD.Experiments
 module Distance =
 
     open System
+    open System.Runtime.InteropServices
     open System.Numerics
     open BenchmarkDotNet.Attributes
 
@@ -26,25 +27,50 @@ module Distance =
 
         // TODO handle cleanly sizes that are not multiples of 4
         let take1 (v1: float[], v2: float[]) =
+
             let size = v1.Length
+            let vSize = Vector<float>.Count
+
             let mutable total = 0.0
-            for i in 0 .. (size - 1) / 4 do
-                let s = 4 * i
-                let v1 = Vector<float>(v1.[s .. s + 3])
-                let v2 = Vector<float>(v2.[s .. s + 3])
+            for i in 0 .. (size - 1) / vSize do
+                let s = vSize * i
+                let v1 = Vector<float>(v1.[s .. s + vSize - 1])
+                let v2 = Vector<float>(v2.[s .. s + vSize - 1])
                 let diff = v1 - v2
                 total <- total + Vector.Dot(diff, diff)
+
             sqrt total
 
         let take2 (v1: float[], v2: float[]) =
+
             let size = v1.Length
+            let vSize = Vector<float>.Count
+
             let s1 = ReadOnlySpan(v1)
             let s2 = ReadOnlySpan(v2)
+
             let mutable total = 0.0
-            for i in 0 .. (size - 1) / 4 do
-                let s = 4 * i
-                let v1 = Vector<float>(s1.Slice(s, 4))
-                let v2 = Vector<float>(s2.Slice(s, 4))
+            for i in 0 .. (size - 1) / vSize do
+                let s = vSize * i
+                let v1 = Vector<float>(s1.Slice(s, vSize))
+                let v2 = Vector<float>(s2.Slice(s, vSize))
+                let diff = v1 - v2
+                total <- total + Vector.Dot(diff, diff)
+
+            sqrt total
+
+        // Feedback from Alexandre Mutel:
+        // https://mastodon.social/@xoofx/113066600743080384
+        // https://gist.github.com/xoofx/2fc4e25ed32732bcfe0559e8c07076bb
+        let take3 (v1: float[], v2: float[]) =
+
+            let s1 = MemoryMarshal.Cast<float, Vector<float>>(ReadOnlySpan(v1))
+            let s2 = MemoryMarshal.Cast<float, Vector<float>>(ReadOnlySpan(v2))
+
+            let mutable total = 0.0
+            for i in 0 .. (s1.Length - 1) do
+                let v1 = s1.[i]
+                let v2 = s2.[i]
                 let diff = v1 - v2
                 total <- total + Vector.Dot(diff, diff)
             sqrt total
@@ -53,8 +79,9 @@ module Distance =
 
         let rng = Random 0
         // Sample vector size
-        // Needs to be a multiple of 4 at the moment.
-        let size = 8 //40_000
+        // Needs to be a multiple of the vector size at the moment.
+        let vSize = Vector<float>.Count
+        let size = 2 * vSize
 
         let v1 = Array.init size (fun _ -> rng.NextDouble())
         let v2 = Array.init size (fun _ -> rng.NextDouble())
@@ -74,3 +101,7 @@ module Distance =
         [<Benchmark>]
         member this.simdV2 () =
             SIMD.take2 (v1, v2)
+
+        [<Benchmark>]
+        member this.simdV3 () =
+            SIMD.take3 (v1, v2)
